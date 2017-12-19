3d99fe03e1baa920487c35f9a73837360379484e
rchandrash <rchandrash@pfsweb.com>
2017-12-19 11:23:11 +0530

    'alter'

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import axios from 'axios';
-    
+
 export default class App extends React.Component {
   state ={
     albums : []
@@ -13,9 +13,12 @@ export default class App extends React.Component {
   getit() {
     return this.state.albums.map(album => <Text key={album.title}>{album.title}</Text>);
   }
-  
+
   render() {
     return (
+      <View>
+      {this.getit()}
+      </View>
       <View>
       {this.getit()}
       </View>
